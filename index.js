e21a33c6fad2179530114675fe80a7f2edfe9a55
valeria100328 <maldonadoperezvaleria@gmail.com>
2022-03-19 16:41:07 -0500

    ProjectExpress

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Los quiero mores<3")
+const express = require ('express')
+const conectar_DataBase = require('./config/database')
+const cors = require('cors')
+//Va a guardar todas las funcionalidades de app en express
+const app = express();
+conectar_DataBase();
+app.use(cors());
+
+app.use(express.json());
+
+
+/*app.get('/',(req,res) => {
+    res.send('Mi aplicación con Express')
+})*/
+app.use('/api/boletaGeneral', require('./routes/boletaGeneral'));
+
+
+app.listen(3000, () => {
+    console.log('El servidor se esta ejecutando en el puerto 3000')
+})
---
+++ routes/boletaGeneral.js
@@ -0,0 +1,22 @@
+const express = require('express');
+//Integraremos todos los métodos http
+const router = express.Router();
+
+
+router.post('/', () => {
+    console.log('Creando Boleta')
+})
+
+router.post('/:id', () => {
+    console.log('Actualizando la Boleta')
+})
+
+router.get('/', () => {
+    console.log('La boleta es tal')
+})
+
+
+
+
+//Tema del router
+module.exports = router;
